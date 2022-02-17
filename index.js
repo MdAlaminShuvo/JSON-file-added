89fd9f3f159e4b67c3a5349087b795babe1ec23d
Alamin <48249531+MdAlaminShuvo@users.noreply.github.com>
2022-02-17 19:58:45 +0600

    json file added

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+const data = require("./friends_f=data.json");
+//console.log(data);
+
+//console.log(data.friends[0]);
+//console.log(data.friends[0].name);
+
+
+// data.friends[0].name = "Anis";
+// console.log(data.friends[0].name);
+
+// delete data.friends[0].name;
+// console.log(data);
+
+
+
+// ///////js to json
+
+// const data2 = {
+//     name : "Alamin",
+//     age : 24
+// };
+// console.log(JSON.stringify(data2));
+
+
+
+///////js to json
+
+
+console.log(JSON.parse('{"name" : "Alamin"}'));
